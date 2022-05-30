5250f46c0307f90c1b6e3b2cb43cb909e41a51a4
Shih-Yuan Lee (FourDollars) <fourdollars@ubuntu.com>
2022-05-31 00:25:06 +0800

    Polish the usage.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI, HTTPException, Form, Header, Request
-from fastapi.responses import Response, PlainTextResponse, RedirectResponse, JSONResponse, HTMLResponse
+from fastapi import FastAPI, HTTPException, Form, Header, Request, Query
+from fastapi.responses import (
+    Response,
+    PlainTextResponse,
+    RedirectResponse,
+    JSONResponse,
+    HTMLResponse,
+)
 from os.path import exists
+from pydantic import Required
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing import Union
 import json
@@ -11,30 +18,26 @@ import urllib.parse
 LAUNCHPAD_URL = "https://launchpad.net"
 LAUNCHPAD_API = "https://api.launchpad.net"
 
-app = FastAPI()
+app = FastAPI(
+    title="Launchpad API Proxy",
+    description="https://github.com/fourdollars/lp-api-proxy/",
+    version="0.0.0",
+    redoc_url="/",
+    # docs_url=None,
+)
 
 
 @app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
+def http_exception_handler(request, exc):
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 
 @app.post("/+request-token", response_class=PlainTextResponse)
-async def request_token(
-    oauth_consumer_key: str = Form(default=None),
-    oauth_signature_method: str = Form(default="PLAINTEXT"),
-    oauth_signature: str = Form(default="&"),
+def request_token(
+    oauth_consumer_key: str = Form(default=Required),
+    oauth_signature_method: str = Form(default=Required),
+    oauth_signature: str = Form(default=Required),
 ):
-    if (
-        oauth_consumer_key is None
-        or oauth_signature_method is None
-        or oauth_signature is None
-    ):
-        raise HTTPException(
-            status_code=404,
-            detail=f"oauth_consumer_key: {oauth_consumer_key}, oauth_signature_method: {oauth_signature_method}, oauth_signature: {oauth_signature}",
-        )
-
     data = {
         "oauth_consumer_key": oauth_consumer_key,
         "oauth_signature_method": oauth_signature_method,
@@ -42,6 +45,7 @@ async def request_token(
     }
 
     response = requests.post(f"{LAUNCHPAD_URL}/+request-token", data=data)
+
     if response.status_code == requests.codes.ok:
         return response.text
 
@@ -49,12 +53,11 @@ async def request_token(
 
 
 @app.get("/+authorize-token", response_class=RedirectResponse)
-async def authorize_token(
-    oauth_token: str = None, allow_permission: str = None, oauth_callback: str = None
+def authorize_token(
+    oauth_token: str = Query(default=Required),
+    allow_permission: str = None,
+    oauth_callback: str = None,
 ):
-    if oauth_token is None:
-        raise HTTPException(status_code=404, detail=f"oauth_token is unavailable.")
-
     params = {"oauth_token": oauth_token}
 
     if allow_permission:
@@ -68,22 +71,11 @@ async def authorize_token(
 
 @app.post("/+access-token", response_class=PlainTextResponse)
 def access_token(
-    oauth_token: str = Form(default=None),
-    oauth_consumer_key: str = Form(default=None),
-    oauth_signature_method: str = Form(default="PLAINTEXT"),
-    oauth_signature: str = Form(default="&"),
+    oauth_token: str = Form(default=Required),
+    oauth_consumer_key: str = Form(default=Required),
+    oauth_signature_method: str = Form(default=Required),
+    oauth_signature: str = Form(default=Required),
 ):
-    if (
-        oauth_token is None
-        or oauth_consumer_key is None
-        or oauth_signature_method is None
-        or oauth_signature is None
-    ):
-        raise HTTPException(
-            status_code=404,
-            detail=f"oauth_token: {oauth_token}, oauth_consumer_key: {oauth_consumer_key}, oauth_signature_method: {oauth_signature_method}, oauth_signature: {oauth_signature}",
-        )
-
     data = {
         "oauth_token": oauth_token,
         "oauth_consumer_key": oauth_consumer_key,
@@ -99,60 +91,87 @@ def access_token(
 
 
 @app.get("/devel/{api:path}", response_class=JSONResponse)
-async def devel_get(request: Request, api: str, authorization: Union[str, None] = Header(default=None)):
+async def devel_get(
+    request: Request, api: str, authorization: Union[str, None] = Header(default=None)
+):
     headers = {}
     if authorization:
         headers["Authorization"] = authorization
-    response = requests.get(f"{LAUNCHPAD_API}/devel/{api}", headers=headers, params=request.query_params)
+    response = requests.get(
+        f"{LAUNCHPAD_API}/devel/{api}", headers=headers, params=request.query_params
+    )
     if response.status_code == requests.codes.ok:
         return json.loads(response.text)
     raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
 @app.post("/devel/{api:path}", response_class=JSONResponse)
-async def devel_post(request: Request, api: str, authorization: Union[str, None] = Header(default=None)):
+async def devel_post(
+    request: Request, api: str, authorization: Union[str, None] = Header(default=None)
+):
     headers = {}
     if authorization:
         headers["Authorization"] = authorization
     payload = await request.form()
-    response = requests.post(f"{LAUNCHPAD_API}/devel/{api}", headers=headers, params=request.query_params, data=payload)
+    response = requests.post(
+        f"{LAUNCHPAD_API}/devel/{api}",
+        headers=headers,
+        params=request.query_params,
+        data=payload,
+    )
     if response.status_code == requests.codes.ok:
         return json.loads(response.text)
     raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
 @app.patch("/devel/{api:path}", response_class=JSONResponse)
-async def devel_patch(request: Request, api: str, authorization: Union[str, None] = Header(default=None)):
+async def devel_patch(
+    request: Request, api: str, authorization: Union[str, None] = Header(default=None)
+):
     headers = {}
     if authorization:
         headers["Authorization"] = authorization
     payload = await request.json()
-    response = requests.patch(f"{LAUNCHPAD_API}/devel/{api}", headers=headers, params=request.query_params, data=json.dumps(payload))
+    response = requests.patch(
+        f"{LAUNCHPAD_API}/devel/{api}",
+        headers=headers,
+        params=request.query_params,
+        data=json.dumps(payload),
+    )
     if response.status_code == requests.codes.ok:
         return json.loads(response.text)
     raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
 @app.put("/devel/{api:path}", response_class=JSONResponse)
-async def devel_put(request: Request, api: str, authorization: Union[str, None] = Header(default=None)):
+async def devel_put(
+    request: Request, api: str, authorization: Union[str, None] = Header(default=None)
+):
     headers = {}
     if authorization:
         headers["Authorization"] = authorization
     payload = await request.json()
-    response = requests.put(f"{LAUNCHPAD_API}/devel/{api}", headers=headers, params=request.query_params, data=json.dumps(payload))
+    response = requests.put(
+        f"{LAUNCHPAD_API}/devel/{api}",
+        headers=headers,
+        params=request.query_params,
+        data=json.dumps(payload),
+    )
     if response.status_code == requests.codes.ok:
         return json.loads(response.text)
     raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
-@app.get("/static/{file}")
-def read_root(file: str):
-    if file == "" or not exists(f"static/{file}"):
+@app.get("/{file}")
+async def read_root(file: str):
+    if file == "" or not exists(f"public/{file}"):
         raise HTTPException(status_code=404)
-    with open(f"static/{file}") as f:
+    with open(f"public/{file}") as f:
         data = f.read()
     if file.endswith(".html"):
         return HTMLResponse(content=data, status_code=requests.codes.ok)
     elif file.endswith(".js"):
-        return Response(content=data, status_code=requests.codes.ok, media_type="text/javascript")
+        return Response(
+            content=data, status_code=requests.codes.ok, media_type="text/javascript"
+        )
     raise HTTPException(status_code=404)
